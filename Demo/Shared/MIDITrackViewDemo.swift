e79b00cb9f09807cdf44972f6961bba9974edaf1
Evan Murray <evan@auraaudio.io>
2022-11-04 18:40:04 -0400

    Add Demo App

---
+++ Demo/Shared/MIDITrackViewDemo.swift
@@ -0,0 +1,33 @@
+// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/MIDITrackView/
+
+import SwiftUI
+import MIDITrackView
+
+struct MIDITrackViewDemo: View {
+    @State var model = MIDITrackViewModel(
+        midiNotes: [
+            MIDINote(position: 80, level: 5, length: 102, height: 5),
+            MIDINote(position: 103, level: 23, length: 75, height: 5),
+            MIDINote(position: 157, level: 36, length: 25, height: 5),
+            MIDINote(position: 208, level: 5, length: 102, height: 5),
+            MIDINote(position: 301, level: 23, length: 75, height: 5),
+            MIDINote(position: 376, level: 36, length: 25, height: 5),
+            MIDINote(position: 402, level: 5, length: 102, height: 5),
+            MIDINote(position: 450, level: 23, length: 75, height: 5),
+            MIDINote(position: 500, level: 36, length: 25, height: 5)
+        ],
+        length: 500,
+        height: 200
+    )
+    public var body: some View {
+        VStack {
+            MIDITrackView(model: $model, trackColor: Color.cyan, noteColor: Color.blue)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MIDITrackViewDemo()
+    }
+}
---
+++ Demo/Shared/MIDITrackViewDemoApp.swift
@@ -0,0 +1,12 @@
+// Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/MIDITrackView/
+
+import SwiftUI
+
+@main
+struct MIDITrackViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MIDITrackViewDemo()
+        }
+    }
+}
