7e699f15359ce5717983922d7b236d62cf31c6a6
Evan Murray <evan@auraaudio.io>
2023-01-08 04:53:55 -0500

    Use `MIDIKit` to read files
    Use the `MIDIKit` Swift Package to handle reading midi files.

--- Demo/Shared/MIDITrackViewDemo.swift
+++ Demo/Shared/MIDITrackViewDemo.swift
@@ -4,21 +4,15 @@ import SwiftUI
 import MIDITrackView
 
 struct MIDITrackViewDemo: View {
-    @State var model = MIDITrackViewModel(
-        midiNotes: [
-            MIDITrackViewNote(position: 80, level: 5, length: 102, height: 5),
-            MIDITrackViewNote(position: 103, level: 23, length: 75, height: 5),
-            MIDITrackViewNote(position: 157, level: 36, length: 25, height: 5),
-            MIDITrackViewNote(position: 208, level: 5, length: 102, height: 5),
-            MIDITrackViewNote(position: 301, level: 23, length: 75, height: 5),
-            MIDITrackViewNote(position: 376, level: 36, length: 25, height: 5),
-            MIDITrackViewNote(position: 402, level: 5, length: 102, height: 5),
-            MIDITrackViewNote(position: 450, level: 23, length: 75, height: 5),
-            MIDITrackViewNote(position: 500, level: 36, length: 25, height: 5)
-        ],
-        length: 606,
-        height: 200
-    )
+    @State var model: MIDITrackViewModel
+
+    public init(midiNotes: [MIDITrackViewNote] = []) {
+        self.model = MIDITrackViewModel(
+            midiNotes: midiNotes,
+            length: 10000,
+            height: 200
+        )
+    }
     public var body: some View {
         VStack {
             MIDITrackView(model: $model, trackColor: Color.cyan, noteColor: Color.blue, note: RoundedRectangle(cornerRadius: 10.0))
--- Demo/Shared/MIDITrackViewDemoApp.swift
+++ Demo/Shared/MIDITrackViewDemoApp.swift
@@ -1,12 +1,90 @@
 // Copyright AudioKit. All Rights Reserved. Revision History at http://github.com/AudioKit/MIDITrackView/
 
 import SwiftUI
+import MIDIKitSMF
+import MIDITrackView
+
+class MIDINoteData {
+    var midiNotes: [MIDITrackViewNote]!
+    init() {
+        let url = Bundle.main.url(forResource: "Demo", withExtension: "mid")! // replace with url to a MIDI file on disk
+        let midiFile = try! MIDIFile(midiFile: url)
+
+        guard case .track(let track) = midiFile.chunks[1] else { return }
+
+        var highNote: UInt7 = 0
+        var lowNote: UInt7 = UInt7.max
+        var noteRange: UInt7 = 0
+        var noteHeight: CGFloat = 0.0
+        var maxHeight: CGFloat = 0.0
+        var noteEvents: [MIDIEvent.NoteOn] = []
+        var noteEventPositions: [UInt32] = []
+        var noteEventLengths: [UInt32] = []
+        var notePosition: UInt32 = 0
+
+        var i = 0
+
+        for event in track.events {
+            switch(event) {
+            case .noteOn(let noteOnDelta, let noteOnEvent):
+                notePosition += noteOnDelta.ticksValue(using: midiFile.timeBase)
+                noteEvents.append(noteOnEvent)
+                noteEventPositions.append(notePosition)
+
+                let events = Array(track.events[i ..< track.events.count])
+
+                let noteOffEvent = events.first { event in
+                    if case .noteOff(let noteOffDelta, let noteOffEvent) = event {
+                        notePosition += noteOffDelta.ticksValue(using: midiFile.timeBase)
+                        return noteOnEvent.note == noteOffEvent.note
+                    } else {
+                        return false
+                    }
+                }
+
+                noteEventLengths.append(noteOffEvent?.smfUnwrappedEvent.delta.ticksValue(using: midiFile.timeBase) ?? 0 - noteOnDelta.ticksValue(using: midiFile.timeBase))
+
+                highNote = (noteOnEvent.note.number > highNote) ? noteOnEvent.note.number : highNote
+                lowNote = (noteOnEvent.note.number < lowNote) ? noteOnEvent.note.number : lowNote
+                noteRange = highNote - lowNote
+                noteHeight = 200.0 / CGFloat(noteRange)
+                maxHeight = 200.0 - noteHeight
+
+                i += 1
+            default:
+                i += 1
+                break
+            }
+        }
+
+        guard noteEvents.count * 2 == noteEventPositions.count + noteEventLengths.count else { return }
+
+        var midiNotes: [MIDITrackViewNote] = []
+
+        i = 0
+
+        for noteEvent in noteEvents {
+            let noteNumber = noteEvent.note.number - lowNote
+            let notePosition = CGFloat(noteEventPositions[i])
+            let noteLength = CGFloat(noteEventLengths[i])
+            let noteLevel = maxHeight - CGFloat(noteNumber) * noteHeight
+
+            let note = MIDITrackViewNote(position: notePosition, level: noteLevel, length: noteLength, height: noteHeight)
+
+            midiNotes.append(note)
+
+            i += 1
+        }
+        self.midiNotes = midiNotes
+    }
+}
 
 @main
 struct MIDITrackViewDemoApp: App {
+    let midiNoteData = MIDINoteData()
     var body: some Scene {
         WindowGroup {
-            MIDITrackViewDemo()
+            MIDITrackViewDemo(midiNotes: midiNoteData.midiNotes)
         }
     }
 }
