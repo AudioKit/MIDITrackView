6de42c5aa5d83f2a6ca8acf7360c3819a7f42add
Evan Murray <evan@auraaudio.io>
2023-01-12 21:39:10 -0500

    Make sequencer play sound!
    Now the sequencer plays sound in the demo when the view is pressed. And yes, I know I'm using the old audio engine. I will update to the new engine once a new release of `AudioKit/AudioKit` is published.

--- Demo/Shared/MIDITrackViewDemo.swift
+++ Demo/Shared/MIDITrackViewDemo.swift
@@ -2,10 +2,24 @@
 
 import SwiftUI
 import MIDITrackView
+import AudioKit
+
+/// A class to manage audio playback within the view
+class Conductor {
+    let midiInstrument = AppleSequencer(fromURL: Bundle.main.url(forResource: "Demo", withExtension: "mid")!)
+    let sampler = MIDISampler()
+    let engine = AudioEngine()
+    init() {
+        midiInstrument.setGlobalMIDIOutput(sampler.midiIn)
+        engine.output = sampler
+        try! engine.start()
+    }
+}
 
 struct MIDITrackViewDemo: View {
     @EnvironmentObject var model: MIDITrackViewModel
     @State private var isPlaying = false
+    let conductor = Conductor()
 
     public var body: some View {
         VStack {
@@ -16,8 +30,10 @@ struct MIDITrackViewDemo: View {
                 .onChange(of: isPlaying) { newValue in
                     if newValue {
                         model.play()
+                        conductor.midiInstrument.play()
                     } else {
                         model.stop()
+                        conductor.midiInstrument.stop()
                     }
                 }
                 .environmentObject(model)
