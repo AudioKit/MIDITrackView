996c2dd1d9d5be2806e3bda9c066491a640f8f42
Evan Murray <evan@auraaudio.io>
2023-12-11 10:07:37 -0500

    Render lots of tracks for craziness

--- Demo/Shared/MIDITrackViewDemo.swift
+++ Demo/Shared/MIDITrackViewDemo.swift
@@ -20,6 +20,33 @@ struct MIDITrackViewDemo: View {
     @StateObject private var chordModel = MIDITrackViewModel()
     @StateObject private var bassModel = MIDITrackViewModel()
     @StateObject private var drumsModel = MIDITrackViewModel()
+    @StateObject private var data1 = MIDITrackViewModel()
+    @StateObject private var data2 = MIDITrackViewModel()
+    @StateObject private var data3 = MIDITrackViewModel()
+    @StateObject private var data4 = MIDITrackViewModel()
+    @StateObject private var data5 = MIDITrackViewModel()
+    @StateObject private var data6 = MIDITrackViewModel()
+    @StateObject private var data7 = MIDITrackViewModel()
+    @StateObject private var data8 = MIDITrackViewModel()
+    @StateObject private var data9 = MIDITrackViewModel()
+    @StateObject private var data10 = MIDITrackViewModel()
+    @StateObject private var data11 = MIDITrackViewModel()
+    @StateObject private var data12 = MIDITrackViewModel()
+    @StateObject private var data13 = MIDITrackViewModel()
+    @StateObject private var data14 = MIDITrackViewModel()
+    @StateObject private var data15 = MIDITrackViewModel()
+    @StateObject private var data16 = MIDITrackViewModel()
+    @StateObject private var data17 = MIDITrackViewModel()
+    @StateObject private var data18 = MIDITrackViewModel()
+    @StateObject private var data19 = MIDITrackViewModel()
+    @StateObject private var data20 = MIDITrackViewModel()
+    @StateObject private var data21 = MIDITrackViewModel()
+    @StateObject private var data22 = MIDITrackViewModel()
+    @StateObject private var data23 = MIDITrackViewModel()
+    @StateObject private var data24 = MIDITrackViewModel()
+    @StateObject private var data25 = MIDITrackViewModel()
+    @StateObject private var data26 = MIDITrackViewModel()
+    @StateObject private var data27 = MIDITrackViewModel()
 
     let conductor = Conductor()
 
@@ -39,6 +66,66 @@ struct MIDITrackViewDemo: View {
                               noteColor: Color.blue,
                               note: RoundedRectangle(cornerRadius: 10.0))
                 MIDITrackView(model: drumsModel,
+                              trackColor: Color.cyan,
+                              noteColor: Color.blue,
+                              note: RoundedRectangle(cornerRadius: 10.0))
+                MIDITrackView(model: data1,
+                              trackColor: Color.cyan,
+                              noteColor: Color.blue,
+                              note: RoundedRectangle(cornerRadius: 10.0))
+                MIDITrackView(model: data2,
+                              trackColor: Color.cyan,
+                              noteColor: Color.blue,
+                              note: RoundedRectangle(cornerRadius: 10.0))
+                MIDITrackView(model: data3,
+                              trackColor: Color.cyan,
+                              noteColor: Color.blue,
+                              note: RoundedRectangle(cornerRadius: 10.0))
+                MIDITrackView(model: data4,
+                              trackColor: Color.cyan,
+                              noteColor: Color.blue,
+                              note: RoundedRectangle(cornerRadius: 10.0))
+                MIDITrackView(model: data5,
+                              trackColor: Color.cyan,
+                              noteColor: Color.blue,
+                              note: RoundedRectangle(cornerRadius: 10.0))
+                MIDITrackView(model: data6,
+                              trackColor: Color.cyan,
+                              noteColor: Color.blue,
+                              note: RoundedRectangle(cornerRadius: 10.0))
+                MIDITrackView(model: data7,
+                              trackColor: Color.cyan,
+                              noteColor: Color.blue,
+                              note: RoundedRectangle(cornerRadius: 10.0))
+                MIDITrackView(model: data8,
+                              trackColor: Color.cyan,
+                              noteColor: Color.blue,
+                              note: RoundedRectangle(cornerRadius: 10.0))
+                MIDITrackView(model: data9,
+                              trackColor: Color.cyan,
+                              noteColor: Color.blue,
+                              note: RoundedRectangle(cornerRadius: 10.0))
+                MIDITrackView(model: data10,
+                              trackColor: Color.cyan,
+                              noteColor: Color.blue,
+                              note: RoundedRectangle(cornerRadius: 10.0))
+                MIDITrackView(model: data11,
+                              trackColor: Color.cyan,
+                              noteColor: Color.blue,
+                              note: RoundedRectangle(cornerRadius: 10.0))
+                MIDITrackView(model: data12,
+                              trackColor: Color.cyan,
+                              noteColor: Color.blue,
+                              note: RoundedRectangle(cornerRadius: 10.0))
+                MIDITrackView(model: data13,
+                              trackColor: Color.cyan,
+                              noteColor: Color.blue,
+                              note: RoundedRectangle(cornerRadius: 10.0))
+                MIDITrackView(model: data14,
+                              trackColor: Color.cyan,
+                              noteColor: Color.blue,
+                              note: RoundedRectangle(cornerRadius: 10.0))
+                MIDITrackView(model: data15,
                               trackColor: Color.cyan,
                               noteColor: Color.blue,
                               note: RoundedRectangle(cornerRadius: 10.0))
@@ -82,6 +169,21 @@ struct MIDITrackViewDemo: View {
         drumsModel.midiNotes = conductor.drumsData.midiNotes
         drumsModel.height = conductor.drumsData.height
         drumsModel.length = conductor.drumsData.length
+        data1.midiNotes = conductor.data5.midiNotes
+        data1.height = conductor.data5.height
+        data1.length = conductor.data5.length
+        data2.midiNotes = conductor.data6.midiNotes
+        data2.height = conductor.data6.height
+        data2.length = conductor.data6.length
+        data3.midiNotes = conductor.data7.midiNotes
+        data3.height = conductor.data7.height
+        data3.length = conductor.data7.length
+        data4.midiNotes = conductor.data8.midiNotes
+        data4.height = conductor.data8.height
+        data4.length = conductor.data8.length
+        data5.midiNotes = conductor.data9.midiNotes
+        data5.height = conductor.data9.height
+        data5.length = conductor.data9.length
     }
 
     func updatePlayer(isPlaying: Bool) {
@@ -100,6 +202,11 @@ struct MIDITrackViewDemo: View {
         chordModel.playPos = beatPos
         bassModel.playPos = beatPos
         drumsModel.playPos = beatPos
+        data1.playPos = beatPos
+        data2.playPos = beatPos
+        data3.playPos = beatPos
+        data4.playPos = beatPos
+        data5.playPos = beatPos
     }
 
     func stopAndRewind() {
